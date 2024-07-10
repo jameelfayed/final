9e3cb1a7d6cf14d6c155b23bec5b21b69f3e8224
jameelfayed <Jameelsameer8888@gmail.com>
2024-07-10 11:13:05 +0300

    Initial Upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/finalProject/",
+  base:"/React-Project/",
 
   plugins: [react()],
 })
